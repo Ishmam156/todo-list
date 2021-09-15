ba140aa6f2d45f37ddc8e8057c870425faa23a21
Ishmam Chowdhury <ishmamchowdhurygp@gmail.com>
2021-09-15 19:05:33 +0600

    localStorage functionality initally working

--- src/script/index.js
+++ src/script/index.js
@@ -5,63 +5,125 @@ import { todo } from "./logic/todo";
 import { project } from "./logic/project";
 import { paintDOM } from "./DOM/appFlow";
 
-const allProjects = [];
-const startProject = project("Starter");
-
-allProjects.push(startProject);
-
-const secondProject = project("Home");
-allProjects.push(secondProject);
-
-const newTodo = todo(
-  "Get Groceries",
-  "Should I do this?",
-  "2021-09-27",
-  "high",
-  startProject.projectID,
+let allProjects = [];
+const checkLocalStorage = JSON.parse(
+  localStorage.getItem("todo-projects"),
 );
-startProject.addTodo(newTodo);
 
-const newTodo2 = todo(
-  "Finish Forecasting",
-  "This is going to change things and I am now doing a big string here, let's see",
-  "2021-09-05",
-  "low",
-  startProject.projectID,
-);
-startProject.addTodo(newTodo2);
+if (checkLocalStorage) {
+  checkLocalStorage.map((item) => {
+    const singleProject = project(item.projectName, item.projectID);
 
-const newTodo3 = todo(
-  "Do Meetings",
-  "Don't really want to",
-  "2021-12-23",
-  "medium",
-  secondProject.projectID,
-);
-secondProject.addTodo(newTodo3);
+    item.todos.forEach((singleTodo) => {
+      singleProject.addTodo(
+        todo(
+          singleTodo.title,
+          singleTodo.description,
+          singleTodo.dueDate,
+          singleTodo.priority,
+          singleTodo.parentID,
+          singleTodo.completion,
+        ),
+      );
+    });
 
-console.log(startProject.toJSON());
-localStorage.setItem("initialProjects", JSON.stringify(startProject));
-const check = JSON.parse(localStorage.getItem("initialProjects"));
-console.log("check", check);
-const checkConvert = project(check.projectName, check.projectID);
+    allProjects.push(singleProject);
+  });
+  // console.log(startProject.toJSON());
+  // localStorage.setItem(
+  //   "initialProjects",
+  //   JSON.stringify(startProject),
+  // );
+  // const check = JSON.parse(localStorage.getItem("initialProjects"));
+  // console.log("check", check);
+  // const checkConvert = project(check.projectName, check.projectID);
+  // check.todos.forEach((item) => {
+  //   checkConvert.addTodo(
+  //     todo(
+  //       item.title,
+  //       item.description,
+  //       item.dueDate,
+  //       item.priority,
+  //       item.parentID,
+  //       item.completion,
+  //     ),
+  //   );
+  // });
+  // console.log(checkConvert);
+  // console.log(checkConvert.getTodo());
+} else {
+  const startProject = project("Starter");
 
-check.todos.forEach((item) => {
-  checkConvert.addTodo(
-    todo(
-      item.title,
-      item.description,
-      item.dueDate,
-      item.priority,
-      item.parentID,
-      item.completion,
-    ),
+  allProjects.push(startProject);
+
+  const secondProject = project("Home");
+  allProjects.push(secondProject);
+
+  const newTodo = todo(
+    "Get Groceries",
+    "Should I do this?",
+    "2021-09-27",
+    "high",
+    startProject.projectID,
   );
-});
+  startProject.addTodo(newTodo);
 
-console.log(checkConvert);
-console.log(checkConvert.getTodo());
+  const newTodo2 = todo(
+    "Finish Forecasting",
+    "This is going to change things and I am now doing a big string here, let's see",
+    "2021-09-05",
+    "low",
+    startProject.projectID,
+  );
+  startProject.addTodo(newTodo2);
 
-paintDOM(checkConvert.projectID);
+  const newTodo3 = todo(
+    "Do Meetings",
+    "Don't really want to",
+    "2021-12-23",
+    "medium",
+    secondProject.projectID,
+  );
+  secondProject.addTodo(newTodo3);
+
+  const projectsToLocalStorage = allProjects.map((project) =>
+    project.toJSON(),
+  );
+
+  localStorage.setItem(
+    "todo-projects",
+    JSON.stringify(projectsToLocalStorage),
+  );
+}
+
+// const allProjects = [];
+
+// console.log(startProject.toJSON());
+// localStorage.setItem("initialProjects", JSON.stringify(startProject));
+// const check = JSON.parse(localStorage.getItem("initialProjects"));
+// console.log("check", check);
+// const checkConvert = project(check.projectName, check.projectID);
+
+// check.todos.forEach((item) => {
+//   checkConvert.addTodo(
+//     todo(
+//       item.title,
+//       item.description,
+//       item.dueDate,
+//       item.priority,
+//       item.parentID,
+//       item.completion,
+//     ),
+//   );
+// });
+
+console.log(allProjects);
+
+const check = allProjects.find(
+  (project) => project.projectName === "Starter",
+);
+console.log(check);
+
+paintDOM(check.projectID);
 
 export { allProjects };
