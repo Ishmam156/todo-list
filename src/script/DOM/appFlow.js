a49a7b7a408c2c82a4ad06378edb5cd17c1f28bd
Ishmam Chowdhury <ishmamchowdhurygp@gmail.com>
2021-09-15 19:13:38 +0600

    localStorage showing up on refresh

--- src/script/DOM/appFlow.js
+++ src/script/DOM/appFlow.js
@@ -1,14 +1,19 @@
 import { displayForms } from "./addForm";
 import { displayAbout } from "./aboutPage";
+import { mainLayout } from "./mainLayout";
 
-const paintDOM = (projectID) => {
+const paintDOM = (projectID, firstVisit) => {
   const logoButton = document.getElementById("logo");
   logoButton.addEventListener("click", () => location.reload());
 
   const aboutButton = document.getElementById("about");
   aboutButton.addEventListener("click", () => displayAbout());
 
-  displayForms(projectID, "first");
+  if (firstVisit) {
+    displayForms(projectID, "first");
+  } else {
+    mainLayout(projectID);
+  }
 };
 
 export { paintDOM };
--- src/script/index.js
+++ src/script/index.js
@@ -9,6 +9,7 @@ let allProjects = [];
 const checkLocalStorage = JSON.parse(
   localStorage.getItem("todo-projects"),
 );
+let firstVisit = false;
 
 if (checkLocalStorage) {
   checkLocalStorage.map((item) => {
@@ -29,62 +30,40 @@ if (checkLocalStorage) {
 
     allProjects.push(singleProject);
   });
-  // console.log(startProject.toJSON());
-  // localStorage.setItem(
-  //   "initialProjects",
-  //   JSON.stringify(startProject),
-  // );
-  // const check = JSON.parse(localStorage.getItem("initialProjects"));
-  // console.log("check", check);
-  // const checkConvert = project(check.projectName, check.projectID);
-  // check.todos.forEach((item) => {
-  //   checkConvert.addTodo(
-  //     todo(
-  //       item.title,
-  //       item.description,
-  //       item.dueDate,
-  //       item.priority,
-  //       item.parentID,
-  //       item.completion,
-  //     ),
-  //   );
-  // });
-  // console.log(checkConvert);
-  // console.log(checkConvert.getTodo());
 } else {
   const startProject = project("Starter");
 
   allProjects.push(startProject);
+  firstVisit = true;
+  // const secondProject = project("Home");
+  // allProjects.push(secondProject);
 
-  const secondProject = project("Home");
-  allProjects.push(secondProject);
+  // const newTodo = todo(
+  //   "Get Groceries",
+  //   "Should I do this?",
+  //   "2021-09-27",
+  //   "high",
+  //   startProject.projectID,
+  // );
+  // startProject.addTodo(newTodo);
 
-  const newTodo = todo(
-    "Get Groceries",
-    "Should I do this?",
-    "2021-09-27",
-    "high",
-    startProject.projectID,
-  );
-  startProject.addTodo(newTodo);
+  // const newTodo2 = todo(
+  //   "Finish Forecasting",
+  //   "This is going to change things and I am now doing a big string here, let's see",
+  //   "2021-09-05",
+  //   "low",
+  //   startProject.projectID,
+  // );
+  // startProject.addTodo(newTodo2);
 
-  const newTodo2 = todo(
-    "Finish Forecasting",
-    "This is going to change things and I am now doing a big string here, let's see",
-    "2021-09-05",
-    "low",
-    startProject.projectID,
-  );
-  startProject.addTodo(newTodo2);
-
-  const newTodo3 = todo(
-    "Do Meetings",
-    "Don't really want to",
-    "2021-12-23",
-    "medium",
-    secondProject.projectID,
-  );
-  secondProject.addTodo(newTodo3);
+  // const newTodo3 = todo(
+  //   "Do Meetings",
+  //   "Don't really want to",
+  //   "2021-12-23",
+  //   "medium",
+  //   secondProject.projectID,
+  // );
+  // secondProject.addTodo(newTodo3);
 
   const projectsToLocalStorage = allProjects.map((project) =>
     project.toJSON(),
@@ -96,27 +75,6 @@ if (checkLocalStorage) {
   );
 }
 
-// const allProjects = [];
-
-// console.log(startProject.toJSON());
-// localStorage.setItem("initialProjects", JSON.stringify(startProject));
-// const check = JSON.parse(localStorage.getItem("initialProjects"));
-// console.log("check", check);
-// const checkConvert = project(check.projectName, check.projectID);
-
-// check.todos.forEach((item) => {
-//   checkConvert.addTodo(
-//     todo(
-//       item.title,
-//       item.description,
-//       item.dueDate,
-//       item.priority,
-//       item.parentID,
-//       item.completion,
-//     ),
-//   );
-// });
-
 console.log(allProjects);
 
 const check = allProjects.find(
@@ -124,6 +82,6 @@ const check = allProjects.find(
 );
 console.log(check);
 
-paintDOM(check.projectID);
+paintDOM(check.projectID, firstVisit);
 
 export { allProjects };
